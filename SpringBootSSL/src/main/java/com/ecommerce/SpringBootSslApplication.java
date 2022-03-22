2b31a6984ef15bf90cb36b314a739ce2bcbb0ff2
Vishal Nalawade <98093583+vishaln007@users.noreply.github.com>
2022-03-22 20:54:18 +0530

    Add files via upload

---
+++ SpringBootSSL/src/main/java/com/ecommerce/SpringBootSslApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSslApplication.class, args);
+	}
+
+}
