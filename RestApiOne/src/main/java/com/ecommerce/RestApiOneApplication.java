0285d648938ebab0ced3133816eb3281fcfd1e61
Vishal Nalawade <98093583+vishaln007@users.noreply.github.com>
2022-03-22 20:50:00 +0530

    Add files via upload

---
+++ RestApiOne/src/main/java/com/ecommerce/RestApiOneApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiOneApplication.class, args);
+	}
+
+}
