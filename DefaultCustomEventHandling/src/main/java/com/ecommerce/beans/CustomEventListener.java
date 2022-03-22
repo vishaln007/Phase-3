c5a14573af3b726808e032a4386fcb10435a6f97
Vishal Nalawade <98093583+vishaln007@users.noreply.github.com>
2022-03-22 20:20:43 +0530

    Add files via upload

---
+++ DefaultCustomEventHandling/src/main/java/com/ecommerce/beans/CustomEventListener.java
@@ -0,0 +1,12 @@
+package com.ecommerce.beans;
+
+import org.springframework.context.ApplicationListener;
+
+public class CustomEventListener implements ApplicationListener<CustomEvent> {
+        
+   public void onApplicationEvent(CustomEvent event) {
+      System.out.println(event.toString());
+   }
+}
+
+
